9b12195f4e46cd9c0abf22a1746931454d79c632
Arpitam Saha <arpitam972@gmail.com>
2023-09-28 09:11:46 +0530

    added new blogs

--- personal-blog/src/Components/Cards/Cards.js
+++ personal-blog/src/Components/Cards/Cards.js
@@ -1,5 +1,9 @@
 import React from 'react';
-import iphone from '../../Images/iphone.jpg';
+import Post1 from '../../Images/iphone.jpg';
+import Post2 from '../../Images/TheAlchemist.jpg';
+import Post3 from '../../Images/G20Summit.jpg';
+import Post4 from '../../Images/RegrettingYou.jpg';
+import Post5 from '../../Images/VandeBharat.jpg';
 import "./Cards.css";
 const Cards = () => {
     return (
@@ -8,62 +12,123 @@ const Cards = () => {
             <div className='cards row'>
 
                 <div className='col-md-4 my-5' >
-                    <div class="card " >
-                        <img src={iphone} alt='Iphone Blog' />
-                        <div class="card-body">
-                            <h5 class="card-title">Latest offering from Apple</h5>
-                            <p class="card-text">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eos dolorem, voluptatum dolores ullam consequuntur reprehenderit, doloribus illum nostrum non cupiditate perferendis, mollitia dignissimos itaque eaque ut sed consequatur. Expedita atque velit, earum vero maxime libero explicabo sequi ipsum commodi, nobis necessitatibus a iste quo voluptate minima ea doloribus unde saepe!</p>
-                            <a href="#" class="btn btn-info">Read Blog</a>
+                    <div className="card " >
+                        <img src={Post1} alt='Iphone Blog' />
+                        <div className="card-body">
+                            <h5 className="card-title">Latest offering from Apple</h5>
+                            <p className="card-text desc">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eos dolorem, voluptatum dolores ullam consequuntur reprehenderit, doloribus illum nostrum non cupiditate perferendis, mollitia dignissimos itaque eaque ut sed consequatur. Expedita atque velit, earum vero maxime libero explicabo sequi ipsum commodi, nobis necessitatibus a iste quo voluptate minima ea doloribus unde saepe!</p>
+                            <a href="#" className="btn btn-info">Read Blog</a>
                         </div>
                     </div>
                 </div>
                 <div className='col-md-4 my-5' >
-                    <div class="card " >
-                        <img src={iphone} alt='Iphone Blog' />
-                        <div class="card-body">
-                            <h5 class="card-title">Latest offering from Apple</h5>
-                            <p class="card-text">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eos dolorem, voluptatum dolores ullam consequuntur reprehenderit, doloribus illum nostrum non cupiditate perferendis, mollitia dignissimos itaque eaque ut sed consequatur. Expedita atque velit, earum vero maxime libero explicabo sequi ipsum commodi, nobis necessitatibus a iste quo voluptate minima ea doloribus unde saepe!</p>
-                            <a href="#" class="btn btn-info">Read Blog</a>
+                    <div className="card " >
+                        <img src={Post2} alt='Alchemist Summary' />
+                        <div className="card-body">
+                            <h5 className="card-title">The Alchemist </h5>
+                            <p className="card-text desc">An Andalusian shepherd boy named Santiago travels from his homeland in Spain to the Egyptian desert in search of a treasure buried in the Pyramids. He is both a simple sheep herder and an ambitious explorer. He follows his dreams, literally, a well as “signs” from his environment. Our main character is thoughtful beyond his years but also a child subject to being impressionable</p>
+                            <a href="#" className="btn btn-info">Read Blog</a>
                         </div>
                     </div>
                 </div>
                 <div className='col-md-4 my-5' >
-                    <div class="card " >
-                        <img src={iphone} alt='Iphone Blog' />
-                        <div class="card-body">
-                            <h5 class="card-title">Latest offering from Apple</h5>
-                            <p class="card-text">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eos dolorem, voluptatum dolores ullam consequuntur reprehenderit, doloribus illum nostrum non cupiditate perferendis, mollitia dignissimos itaque eaque ut sed consequatur. Expedita atque velit, earum vero maxime libero explicabo sequi ipsum commodi, nobis necessitatibus a iste quo voluptate minima ea doloribus unde saepe!</p>
-                            <a href="#" class="btn btn-info">Read Blog</a>
+                    <div className="card " >
+                        <img src={Post3} alt='Iphone Blog' />
+                        <div className="card-body">
+                            <h5 className="card-title">Latest offering from Apple</h5>
+                            <p className="card-text desc">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eos dolorem, voluptatum dolores ullam consequuntur reprehenderit, doloribus illum nostrum non cupiditate perferendis, mollitia dignissimos itaque eaque ut sed consequatur. Expedita atque velit, earum vero maxime libero explicabo sequi ipsum commodi, nobis necessitatibus a iste quo voluptate minima ea doloribus unde saepe!</p>
+                            <a href="#" className="btn btn-info">Read Blog</a>
                         </div>
                     </div>
                 </div>
                 <div className='col-md-4 my-5' >
-                    <div class="card " >
-                        <img src={iphone} alt='Iphone Blog' />
-                        <div class="card-body">
-                            <h5 class="card-title">Latest offering from Apple</h5>
-                            <p class="card-text">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eos dolorem, voluptatum dolores ullam consequuntur reprehenderit, doloribus illum nostrum non cupiditate perferendis, mollitia dignissimos itaque eaque ut sed consequatur. Expedita atque velit, earum vero maxime libero explicabo sequi ipsum commodi, nobis necessitatibus a iste quo voluptate minima ea doloribus unde saepe!</p>
-                            <a href="#" class="btn btn-info">Read Blog</a>
+                    <div className="card " >
+                        <img src={Post4} alt='Iphone Blog' />
+                        <div className="card-body">
+                            <h5 className="card-title">Latest offering from Apple</h5>
+                            <p className="card-text desc">Morgan Grant and her sixteen-year-old daughter, Clara, would like nothing more than to be nothing alike. Morgan is determined to prevent her daughter from making the same mistakes she did. By getting pregnant and married way too young, Morgan put her own dreams on hold.</p>
+                            <a href="#" className="btn btn-info">Read Blog</a>
                         </div>
                     </div>
                 </div>
                 <div className='col-md-4 my-5' >
-                    <div class="card " >
-                        <img src={iphone} alt='Iphone Blog' />
-                        <div class="card-body">
-                            <h5 class="card-title">Latest offering from Apple</h5>
-                            <p class="card-text">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eos dolorem, voluptatum dolores ullam consequuntur reprehenderit, doloribus illum nostrum non cupiditate perferendis, mollitia dignissimos itaque eaque ut sed consequatur. Expedita atque velit, earum vero maxime libero explicabo sequi ipsum commodi, nobis necessitatibus a iste quo voluptate minima ea doloribus unde saepe!</p>
-                            <a href="#" class="btn btn-info">Read Blog</a>
+                    <div className="card " >
+                        <img src={Post5} alt='Iphone Blog' />
+                        <div className="card-body">
+                            <h5 className="card-title">Latest offering from Apple</h5>
+                            <p className="card-text desc">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eos dolorem, voluptatum dolores ullam consequuntur reprehenderit, doloribus illum nostrum non cupiditate perferendis, mollitia dignissimos itaque eaque ut sed consequatur. Expedita atque velit, earum vero maxime libero explicabo sequi ipsum commodi, nobis necessitatibus a iste quo voluptate minima ea doloribus unde saepe!</p>
+                            <a href="#" className="btn btn-info">Read Blog</a>
                         </div>
                     </div>
                 </div>
                 <div className='col-md-4 my-5' >
-                    <div class="card " >
-                        <img src={iphone} alt='Iphone Blog' />
-                        <div class="card-body">
-                            <h5 class="card-title">Latest offering from Apple</h5>
-                            <p class="card-text">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eos dolorem, voluptatum dolores ullam consequuntur reprehenderit, doloribus illum nostrum non cupiditate perferendis, mollitia dignissimos itaque eaque ut sed consequatur. Expedita atque velit, earum vero maxime libero explicabo sequi ipsum commodi, nobis necessitatibus a iste quo voluptate minima ea doloribus unde saepe!</p>
-                            <a href="#" class="btn btn-info">Read Blog</a>
+                    <div className="card " >
+                        <img src={Post3} alt='Iphone Blog' />
+                        <div className="card-body">
+                            <h5 className="card-title">Latest offering from Apple</h5>
+                            <p className="card-text desc">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eos dolorem, voluptatum dolores ullam consequuntur reprehenderit, doloribus illum nostrum non cupiditate perferendis, mollitia dignissimos itaque eaque ut sed consequatur. Expedita atque velit, earum vero maxime libero explicabo sequi ipsum commodi, nobis necessitatibus a iste quo voluptate minima ea doloribus unde saepe!</p>
+                            <a href="#" className="btn btn-info">Read Blog</a>
+                        </div>
+                    </div>
+                </div>
+
+                <div className='col-md-4 my-5' >
+                    <div className="card " >
+                        <img src={Post1} alt='Iphone Blog' />
+                        <div className="card-body">
+                            <h5 className="card-title">Latest offering from Apple</h5>
+                            <p className="card-text desc">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eos dolorem, voluptatum dolores ullam consequuntur reprehenderit, doloribus illum nostrum non cupiditate perferendis, mollitia dignissimos itaque eaque ut sed consequatur. Expedita atque velit, earum vero maxime libero explicabo sequi ipsum commodi, nobis necessitatibus a iste quo voluptate minima ea doloribus unde saepe!</p>
+                            <a href="#" className="btn btn-info">Read Blog</a>
+                        </div>
+                    </div>
+                </div>
+                <div className='col-md-4 my-5' >
+                    <div className="card " >
+                        <img src={Post2} alt='Alchemist Summary' />
+                        <div className="card-body">
+                            <h5 className="card-title">The Alchemist </h5>
+                            <p className="card-text desc">An Andalusian shepherd boy named Santiago travels from his homeland in Spain to the Egyptian desert in search of a treasure buried in the Pyramids. He is both a simple sheep herder and an ambitious explorer. He follows his dreams, literally, a well as “signs” from his environment. Our main character is thoughtful beyond his years but also a child subject to being impressionable</p>
+                            <a href="#" className="btn btn-info">Read Blog</a>
+                        </div>
+                    </div>
+                </div>
+                <div className='col-md-4 my-5' >
+                    <div className="card " >
+                        <img src={Post3} alt='Iphone Blog' />
+                        <div className="card-body">
+                            <h5 className="card-title">Latest offering from Apple</h5>
+                            <p className="card-text desc">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eos dolorem, voluptatum dolores ullam consequuntur reprehenderit, doloribus illum nostrum non cupiditate perferendis, mollitia dignissimos itaque eaque ut sed consequatur. Expedita atque velit, earum vero maxime libero explicabo sequi ipsum commodi, nobis necessitatibus a iste quo voluptate minima ea doloribus unde saepe!</p>
+                            <a href="#" className="btn btn-info">Read Blog</a>
+                        </div>
+                    </div>
+                </div>
+                <div className='col-md-4 my-5' >
+                    <div className="card " >
+                        <img src={Post4} alt='Iphone Blog' />
+                        <div className="card-body">
+                            <h5 className="card-title">Latest offering from Apple</h5>
+                            <p className="card-text desc">Morgan Grant and her sixteen-year-old daughter, Clara, would like nothing more than to be nothing alike. Morgan is determined to prevent her daughter from making the same mistakes she did. By getting pregnant and married way too young, Morgan put her own dreams on hold.</p>
+                            <a href="#" className="btn btn-info">Read Blog</a>
+                        </div>
+                    </div>
+                </div>
+                <div className='col-md-4 my-5' >
+                    <div className="card " >
+                        <img src={Post5} alt='Iphone Blog' />
+                        <div className="card-body">
+                            <h5 className="card-title">Latest offering from Apple</h5>
+                            <p className="card-text desc">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eos dolorem, voluptatum dolores ullam consequuntur reprehenderit, doloribus illum nostrum non cupiditate perferendis, mollitia dignissimos itaque eaque ut sed consequatur. Expedita atque velit, earum vero maxime libero explicabo sequi ipsum commodi, nobis necessitatibus a iste quo voluptate minima ea doloribus unde saepe!</p>
+                            <a href="#" className="btn btn-info">Read Blog</a>
+                        </div>
+                    </div>
+                </div>
+                <div className='col-md-4 my-5' >
+                    <div className="card " >
+                        <img src={Post3} alt='Iphone Blog' />
+                        <div className="card-body">
+                            <h5 className="card-title">Latest offering from Apple</h5>
+                            <p className="card-text desc">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eos dolorem, voluptatum dolores ullam consequuntur reprehenderit, doloribus illum nostrum non cupiditate perferendis, mollitia dignissimos itaque eaque ut sed consequatur. Expedita atque velit, earum vero maxime libero explicabo sequi ipsum commodi, nobis necessitatibus a iste quo voluptate minima ea doloribus unde saepe!</p>
+                            <a href="#" className="btn btn-info">Read Blog</a>
                         </div>
                     </div>
                 </div>
--- personal-blog/src/Footer/Footer.js
+++ personal-blog/src/Footer/Footer.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import "./footer.css";
+import "./Footer.css";
 const Footer = () => {
     return (
         <div>
             <footer class="py-3 my-4">
                 <hr />
-                <p className="text-center text-muted">© 2021 BlogUp</p>
+                <p className="text-center text-muted">© 2021 <a href='#'>DeathNote</a></p>
                 <p className='text-center text-muted'>Made with ❤️ by <a href=''>Arpitam Saha</a></p>
             </footer>
         </div>
